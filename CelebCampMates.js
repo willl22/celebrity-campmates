db30e2f06f17654a29eb2904e4533d9a08560cca
WillL <143326106+willl22@users.noreply.github.com>
2023-09-30 11:07:31 +0100

    add randomiser functions for arrays

--- CelebCampMates.js
+++ CelebCampMates.js
@@ -3,3 +3,7 @@ const jobs2 = ['builder', 'strategist', 'defender'];
 const jobs3 = ['emotional support', 'explorer', 'lazy good-for-nothing'];
 
 const names = ['Dwayne Johnson', 'Bear Grylls', 'Jack Black', 'Scarlett Johansson', 'Kim Kardashian', 'Beyonce', 'Ryan Reynolds', 'Stephen King', 'Kathy Bates', 'Hannah Waddingham'];
+
+const randPerc = () => Math.floor(Math.random() * 100);
+const randJob = jobArr => jobArr[Math.floor(Math.random() * 3)];
+const randName = nameArr => nameArr[Math.floor(Math.random() * 10)];
