3e55c2d46ba63c070078d555d5069f05f85c68e4
WillL <143326106+willl22@users.noreply.github.com>
2023-09-30 10:49:29 +0100

    add jobs and names arrays

---
+++ CelebCampMates.js
@@ -0,0 +1,5 @@
+const jobs1 = ['scavenger', 'hunter', 'chef'];
+const jobs2 = ['builder', 'strategist', 'defender'];
+const jobs3 = ['emotional support', 'explorer', 'lazy good-for-nothing'];
+
+const names = ['Dwayne Johnson', 'Bear Grylls', 'Jack Black', 'Scarlett Johansson', 'Kim Kardashian', 'Beyonce', 'Ryan Reynolds', 'Stephen King', 'Kathy Bates', 'Hannah Waddingham'];
