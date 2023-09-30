145c1f284cc6db86ff7df42373f314738702e7c9
WillL <143326106+willl22@users.noreply.github.com>
2023-09-30 11:28:30 +0100

    assemble the functions to create the output message

--- CelebCampMates.js
+++ CelebCampMates.js
@@ -7,3 +7,24 @@ const names = ['Dwayne Johnson', 'Bear Grylls', 'Jack Black', 'Scarlett Johansso
 const randPerc = () => Math.floor(Math.random() * 100);
 const randJob = jobArr => jobArr[Math.floor(Math.random() * 3)];
 const randName = nameArr => nameArr[Math.floor(Math.random() * 10)];
+
+const createTeam = () => {
+    console.log(`
+    Oh no! You're trapped on a deserted tropical island!
+    But don't worry, a bunch of celebrities are with you too!
+    You've assigned them the following roles:
+    
+    Your ${randJob(jobs1)} is ${randName(names)}
+    
+    Your ${randJob(jobs2)} is ${randName(names)}
+    
+    Your ${randJob(jobs3)} is ${randName(names)}
+    
+    What a team! Your chance of survival is:
+    ${randPerc()}%
+    `);
+    
+
+};
+
+createTeam();
